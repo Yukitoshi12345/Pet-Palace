3631974132e536bbae2927f767ee951066e2e058
Jodie <jodielee062788@gmail.com>
2024-04-30 23:36:18 +1000

    add other fields in signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    username: '',
+    name: '',
+    birthday: '',
     email: '',
     password: '',
+    confirmPassword: '',
   });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser, { data }] = useMutation(ADD_USER);
+  const [errorMsg, setErrorMsg] = useState('');
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -24,6 +26,12 @@ const Signup = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    if (formState.password !== formState.confirmPassword) {
+      // If passwords don't match, set an error message and return early
+      setErrorMsg('Passwords do not match');
+      return;
+    }
+
     try {
       const { data } = await addUser({
         variables: { ...formState },
@@ -36,33 +44,43 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex justify-center mt-10">
+    <main className="flex justify-center">
       <div className="max-w-md w-full px-4 mt-20">
         <div className="bg-white shadow-md rounded px-8 py-8">
-          <h4 className="text-2xl mb-4 font-bold text-black">SIGN UP</h4>
+          <h4 className="text-2xl mb-4 font-bold text-black text-center">SIGN UP</h4>
           {data ? (
             <p className="text-green-500 mb-4">
-              Success! You may now head{' '}
-              <Link to="/" className="text-blue-500 hover:underline">
-                back to the homepage.
-              </Link>
+              Logged in succesfully!
             </p>
           ) : (
             <form onSubmit={handleFormSubmit}>
-              <div className="mb-4">
+               <div className="mb-4">
+               <label htmlFor="username" className="block text-black mb-2">Name</label>
                 <input
                   className="w-full px-3 py-2 border rounded"
-                  placeholder="Your username"
-                  name="username"
+                  placeholder="Enter full name"
+                  name="fullName"
                   type="text"
-                  value={formState.username}
+                  value={formState.fullName}
                   onChange={handleChange}
                 />
               </div>
               <div className="mb-4">
+              <label htmlFor="username" className="block text-black mb-2">Birthday</label>
                 <input
                   className="w-full px-3 py-2 border rounded"
-                  placeholder="Your email"
+                  placeholder="Enter birthday"
+                  name="birthday"
+                  type="date"
+                  value={formState.birthday}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+              <label htmlFor="username" className="block text-black mb-2">Email</label>
+                <input
+                  className="w-full px-3 py-2 border rounded"
+                  placeholder="Enter email"
                   name="email"
                   type="email"
                   value={formState.email}
@@ -70,26 +88,37 @@ const Signup = () => {
                 />
               </div>
               <div className="mb-4">
+              <label htmlFor="username" className="block text-black mb-2">Password</label>
                 <input
                   className="w-full px-3 py-2 border rounded"
-                  placeholder="******"
+                  placeholder="Must have at least 5 characters"
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
               </div>
+              <div className="mb-4">
+              <label htmlFor="username" className="block text-black mb-2">Confirm Password</label>
+                <input
+                  className="w-full px-3 py-2 border rounded"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  type="password"
+                  value={formState.confirmPassword}
+                  onChange={handleChange}
+                />
+              </div>
               <button
-                className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                style={{ cursor: 'pointer' }}
+                className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
                 type="submit"
               >
                 REGISTER
               </button>
             </form>
           )}
-          {error && (
-            <div className="text-red-500 mt-4">{error.message}</div>
+          {errorMsg && (
+            <div className="text-red-500 mt-4">{errorMsg}</div>
           )}
         </div>
       </div>
