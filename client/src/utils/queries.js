fe1e25c4e6bdbc6a5bec8dd45a629afec4849ce8
Jodie <jodielee062788@gmail.com>
2024-04-30 22:19:18 +1000

    fix queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,11 @@
 import { gql } from '@apollo/client';
 
-export const GET_USER = gql`
-    query user {
-        user {
-            _id
-            name
-            birthday
-            email
-        }
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email  
     }
+  }
 `;
