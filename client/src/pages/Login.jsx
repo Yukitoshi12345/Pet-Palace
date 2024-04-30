9f0ca0cf6e565cb9f0b5fd4a8001c115d0203fb1
Jodie <jodielee062788@gmail.com>
2024-04-30 18:27:21 +1000

    Edit login and sign up button in navbar

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,40 +1,40 @@
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
+// import { useState } from 'react';
+// import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+// import { LOGIN } from '../utils/mutations';
+// import Auth from '../utils/auth';
 
 function Login() {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+  // const [formState, setFormState] = useState({ email: '', password: '' });
+  // const [login, { error }] = useMutation(LOGIN);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     const mutationResponse = await login({
+  //       variables: { email: formState.email, password: formState.password },
+  //     });
+  //     const token = mutationResponse.data.login.token;
+  //     Auth.login(token);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
 
   return (
     <div className="container my-1">
       <Link to="/signup">← Go to Signup</Link>
 
       <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
+      {/* <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
           <label htmlFor="email">Email address:</label>
           <input
@@ -63,7 +63,7 @@ function Login() {
         <div className="flex-row flex-end">
           <button type="submit">Submit</button>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,41 +1,41 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+// import { useMutation } from '@apollo/client';
+// import Auth from '../utils/auth';
+// import { ADD_USER } from '../utils/mutations';
 
 function Signup() {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [addUser] = useMutation(ADD_USER);
+  // const [formState, setFormState] = useState({ email: '', password: '' });
+  // const [addUser] = useMutation(ADD_USER);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-      },
-    });
-    const token = mutationResponse.data.addUser.token;
-    Auth.login(token);
-  };
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const mutationResponse = await addUser({
+  //     variables: {
+  //       email: formState.email,
+  //       password: formState.password,
+  //       firstName: formState.firstName,
+  //       lastName: formState.lastName,
+  //     },
+  //   });
+  //   const token = mutationResponse.data.addUser.token;
+  //   Auth.login(token);
+  // };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
 
   return (
     <div className="container my-1">
       <Link to="/login">← Go to Login</Link>
 
       <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
+      {/* <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
           <label htmlFor="firstName">First Name:</label>
           <input
@@ -79,7 +79,7 @@ function Signup() {
         <div className="flex-row flex-end">
           <button type="submit">Submit</button>
         </div>
-      </form>
+      </form> */}
     </div>
   );
 }
