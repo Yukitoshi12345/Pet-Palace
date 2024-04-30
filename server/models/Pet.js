f5762dab1402669e7f7f98bdc14d2604465e805d
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-04-30 21:05:25 +1000

    pet.js model complete

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -1,41 +1,78 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
 
-const petSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const petSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: [true, 'Type of pet is required'],
+      enum: ['Dog', 'Cat', 'Hamster', 'Bird', 'Rabbit'],
+    },
+    variety: {
+      // Generic term that can refer to breed or species
+      type: String,
+      required: [
+        true,
+        'Breed or species is required depending on the type of pet',
+      ],
+    },
+    gender: {
+      type: String,
+      required: [true, 'Gender is required'],
+      enum: ['Male', 'Female', 'Other'],
+    },
+    age: {
+      type: Number,
+      required: [true, 'Age is required'],
+      min: [0, 'Age must be a positive number'],
+    },
+    color: {
+      type: String,
+      required: [true, 'Color is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+    location: {
+      type: String,
+      required: [true, 'Location is required'],
+    },
+    health: {
+      type: String,
+      required: [false, 'Health status is optional'],
+    },
+    tame: {
+      type: Boolean,
+      required: [false, 'Tameness is optional'],
+      default: false,
+    },
+    specialNeeds: {
+      type: String,
+      required: [false, 'Special needs are optional'],
+    },
+    vaccinationHistory: {
+      type: String,
+      required: [true, 'Vaccination history is required'],
+    },
+    disability: {
+      type: String,
+      default: 'None',
+    },
+    pedigreeKnown: {
+      type: Boolean,
+      default: false,
+    },
   },
-  gender: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  age: {
-    type: Number,
-    required: true,
-    min: 0.99,
-  },
-  breed: {
-    type: String,
-  },
-  species: {
-    type: String,
-  },
-  color: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-
-  // TODO: yet to complete
-});
+);
 
 const Pet = mongoose.model('Pet', petSchema);
 
