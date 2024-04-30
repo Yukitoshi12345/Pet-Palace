53f7f2d7f37344eef19980bc16e22662e9890611
Jodie <jodielee062788@gmail.com>
2024-05-01 00:59:11 +1000

    set up api routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
+const router = require('express').Router();
 const path = require('path');
+const apiRoutes = require('./api');
 
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-};
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+module.exports = router;
