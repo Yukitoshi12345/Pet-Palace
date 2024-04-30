483f84390a3cd8de6983463e354dc7dd1160ad03
SimpleSuyash <suyashmaharjan@gmail.com>
2024-04-30 13:04:02 +1000

    Navbar is finalized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import ErrorPage from './pages/ErrorPage'
 
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'services',
+        element: <Services />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
