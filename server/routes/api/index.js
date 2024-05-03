e7a5718cd4b53e7a3ac17badf4a1c52c770ecd7c
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-05-03 11:23:07 +1000

    updated stripe

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const paymentRoutes = require('./payment-routes');
 
 router.use('/users', userRoutes);
+router.use('/payments', paymentRoutes);
 
 module.exports = router;
--- server/routes/api/payment-routes.js
+++ server/routes/api/payment-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.Stripe_API_KEY);
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 router.post('/create-charge', async (req, res) => {
   try {
