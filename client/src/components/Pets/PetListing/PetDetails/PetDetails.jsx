4484e94bbe520f2184ea6823a1960a8b293b9669
Jodie <jodielee062788@gmail.com>
2024-05-06 00:25:53 +1000

    update pet details ui and resize images

--- client/src/components/Pets/PetListing/PetDetails/PetDetails.jsx
+++ client/src/components/Pets/PetListing/PetDetails/PetDetails.jsx
@@ -22,54 +22,60 @@ const PetDetails = () => {
   }
 
   return (
-    <div className="container mx-auto mt-20 section">
-      <section id="profile" className="max-w-lg mx-auto bg-neutral rounded-lg shadow-lg p-8 mt-20">
-        <h1 className="text-3xl font-bold mb-4">
-          {pet.name}
-          {pet.featured && <span className="badge badge-secondary py-3 ml-4 rounded-xl">FEATURED</span>}
-          {pet.tame && <span className="badge badge-secondary py-3 ml-4 rounded-xl">TAME</span>}
-          {pet.pedigreeKnown && <span className="badge badge-secondary py-3 ml-4 rounded-xl">PEDIGREE KNOWN</span>}
-        </h1>
-        <img className="rounded-lg mb-4" src={`/images/pets/${petType}s/${pet.photo}`} alt={pet.name} />
-        <p className="text-base-100"><span className="text-base-100 font-bold">Type: </span>{pet.type}</p>
-        <p className="text-base-100"><span className="text-base-100 font-bold">Color: </span>{pet.color}</p>
-        <p className="text-base-100"><span className="text-base-100 font-bold">Age: </span>{pet.age}</p>
-        <p className="text-base-100"><span className="text-base-100 font-bold">Gender: </span>{pet.gender}</p>
-        {petType === 'bird' && (
-          <div>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Species: </span>{pet.species}</p>
-          </div>
-        )}
-        {petType !== 'bird' && (
-          <div>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Breed: </span>{pet.breed}</p>
-          </div>
-        )}
-        
-        <p className="text-base-100"><span className="text-base-100 font-bold">Location: </span>{pet.location}</p>
-        <p className="text-base-100"><span className="text-base-100 font-bold">Description: </span>{pet.description}</p>     
-
-        {(petType === 'bird' || petType === 'hamster') && (
-          <div>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Health: </span>{pet.health}</p>
-          </div>
-
-        )}
-        {petType === 'bird' && (
-          <div>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Special Needs: </span>{pet.specialNeeds}</p>
-          </div>
-        )}
-
-        {(petType === 'cat' || petType === 'dog' || petType === 'rabbit') && (
-          <div>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Vaccination History: </span>{pet.vaccinationHistory}</p>
-            <p className="text-base-100"><span className="text-base-100 font-bold">Disability: </span>{pet.disability}</p>
-          </div>
-        )}
-        <div className="text-center mt-8">
-          <NavLink to="/enquiry" className="btn btn-primary">Enquire Now</NavLink>
+    <div className="container mx-auto section">
+      <section id="petDetails" className="mx-auto bg-neutral rounded-lg shadow-lg p-8">
+        <div>
+          <h1 className="text-3xl font-bold mb-1">
+            {pet.name}
+            {pet.featured && <span className="badge badge-secondary py-3 ml-4 rounded-xl">FEATURED</span>}
+            {pet.tame && <span className="badge badge-secondary py-3 ml-4 rounded-xl">TAME</span>}
+            {pet.pedigreeKnown && <span className="badge badge-secondary py-3 ml-4 rounded-xl">PEDIGREE KNOWN</span>}
+          </h1>
         </div>
+        <div className='grid grid-cols-2'>
+          <div>
+              <img className="rounded-lg" src={`/images/pets/${petType}s/${pet.photo}`} alt={pet.name} style={{ width: '650px', height: 'auto' }} />
+          </div>
+          <div>
+            <p className="text-base-100"><span className="text-base-100 font-bold">Type: </span>{pet.type}</p>
+            <p className="text-base-100"><span className="text-base-100 font-bold">Color: </span>{pet.color}</p>
+            <p className="text-base-100"><span className="text-base-100 font-bold">Age: </span>{pet.age}</p>
+            <p className="text-base-100"><span className="text-base-100 font-bold">Gender: </span>{pet.gender}</p>
+            {petType === 'bird' && (
+              <div>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Species: </span>{pet.species}</p>
+              </div>
+            )}
+            {petType !== 'bird' && (
+              <div>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Breed: </span>{pet.breed}</p>
+              </div>
+            )}
+            <p className="text-base-100"><span className="text-base-100 font-bold">Location: </span>{pet.location}</p>
+            <p className="text-base-100"><span className="text-base-100 font-bold">Description: </span>{pet.description}</p>     
+
+            {(petType === 'bird' || petType === 'hamster') && (
+              <div>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Health: </span>{pet.health}</p>
+              </div>
+            )}
+            {petType === 'bird' && (
+              <div>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Special Needs: </span>{pet.specialNeeds}</p>
+              </div>
+            )}
+
+            {(petType === 'cat' || petType === 'dog' || petType === 'rabbit') && (
+              <div>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Vaccination History: </span>{pet.vaccinationHistory}</p>
+                <p className="text-base-100"><span className="text-base-100 font-bold">Disability: </span>{pet.disability}</p>
+              </div>
+            )}
+          <div className="text-center">
+            <NavLink to="/enquiry" className="btn btn-primary rounded btn-block">Enquire Now</NavLink>
+          </div>
+          </div>
+        </div>  
       </section>
     </div>
   );
