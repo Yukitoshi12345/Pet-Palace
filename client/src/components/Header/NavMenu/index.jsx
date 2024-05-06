70ad23efe8736a50924f0e50b24905ba9510960b
Jodie <jodielee062788@gmail.com>
2024-05-06 20:06:51 +1000

    Edit navmenu

--- client/src/components/Header/NavMenu/index.jsx
+++ client/src/components/Header/NavMenu/index.jsx
@@ -1,7 +1,7 @@
-import { header } from "../../../data";
-import { NavLink } from "react-router-dom";
-import Auth from '../../../utils/auth'; 
 import { useQuery } from '@apollo/client';
+import { NavLink } from 'react-router-dom';
+import Auth from '../../../utils/auth';
+import { header } from '../../../data';
 import { QUERY_SINGLE_USER } from '../../../utils/queries';
 
 const NavMenu = ({ userId, bg }) => {
@@ -16,12 +16,10 @@ const NavMenu = ({ userId, bg }) => {
     Auth.logout();
   };
 
-  const { navigation } = header;
-
   return (
     <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="navbar-sticky">
       <ul className={`${bg ? "lg:bg-base-200" : "lg:bg-base-100"} bg-base-300 capitalize flex flex-col p-4 lg:p-0 mt-4 font-medium border rounded-lg lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0`}>
-        {navigation.map((navItem, index) => (
+        {header.navigation.map((navItem, index) => ( 
           <li
             key={index}
             className="block py-2 px-3 rounded hover:text-orange-700 lg:p-0"
@@ -31,30 +29,7 @@ const NavMenu = ({ userId, bg }) => {
             </NavLink>
           </li>
         ))}
-        {Auth.loggedIn() && !loading && (
-          <>
-
-            {user ? (
-              <li className="block py-2 px-3 rounded hover:text-orange-700 lg:p-0">
-                <NavLink to={`/profiles/${user._id}`}>Profile</NavLink>
-              </li>
-            ) : null}
-            <li className="block py-2 px-3 rounded hover:text-orange-700 lg:p-0">
-              <button onClick={logout}>Logout</button>
-            </li>
-
-          </>
-        )}
-        {!Auth.loggedIn() && !loading && (
-          <>
-            <li className="block py-2 px-3 rounded hover:text-orange-700 lg:p-0">
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li className="block py-2 px-3 rounded hover:text-orange-700 lg:p-0">
-              <NavLink to="/signup">Signup</NavLink>
-            </li>
-          </>
-        )}
+       
       </ul>
     </div>
   );
