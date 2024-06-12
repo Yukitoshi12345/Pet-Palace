0606ab9f070d2f9a96cf01bb2fc02fb236016c46
Jodie <jodielee062788@gmail.com>
2024-06-12 14:12:34 +1000

    update donation page

--- client/src/pages/Donate.jsx
+++ client/src/pages/Donate.jsx
@@ -46,54 +46,54 @@ const Donate = () => {
       <div className="flex items-center justify-center p-6 flex-grow">
         <div className="w-full max-w-7xl">
           <h2 className="text-[35px] font-bold text-center mb-6 border-b p-3">DONATION</h2>
-          <p className="text-center mb-6 text-lg">
+          <p className="text-center mb-12 text-lg">
             Your donations help us provide shelter, food, and medical care for abandoned and rescued animals. By contributing to our shelter, you are helping us give these animals a second chance at life. Every donation, no matter how small, makes a huge difference. Donate wholeheartedly and receive a free gift pack as a token of our appreciation!
           </p>
           {error && (
             <p className="text-red-500 text-center mb-4">Error during donation. Please try again later.</p>
           )}
           <div className="flex justify-center mb-6 space-x-4">
-            <div className="card p-6 border rounded-lg shadow-lg bg-white">
+            <div className="card p-6 border rounded-lg shadow-lg bg-neutral">
               <h3 className="text-xl font-bold mb-4 text-center">Donation Amount</h3>
-              <p className="text-lg mb-4 text-black text-center">Donate $5</p>
+              <p className="text-lg mb-4 text-base-100 text-center">$5</p>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
+                className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-orange-600 w-full"
                 onClick={() => handleDonate(5)}
               >
                 Donate
               </button>
             </div>
-            <div className="card p-6 border rounded-lg shadow-lg bg-white">
+            <div className="card p-6 border rounded-lg shadow-lg bg-neutral">
               <h3 className="text-xl font-bold mb-4 text-center">Donation Amount</h3>
-              <p className="text-lg mb-4 text-black text-center">Donate $10</p>
+              <p className="text-lg mb-4 text-base-100 text-center">$10</p>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
+                className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-orange-600 w-full"
                 onClick={() => handleDonate(10)}
               >
                 Donate
               </button>
             </div>
-            <div className="card p-6 border rounded-lg shadow-lg bg-white">
+            <div className="card p-6 border rounded-lg shadow-lg bg-neutral">
               <h3 className="text-xl font-bold mb-4 text-center">Donation Amount</h3>
-              <p className="text-lg mb-4 text-black text-center">Donate $15</p>
+              <p className="text-lg mb-4 text-base-100 text-center">$15</p>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
+                className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-orange-600 w-full"
                 onClick={() => handleDonate(15)}
               >
                 Donate
               </button>
             </div>
-            <div className="card p-6 border rounded-lg shadow-lg bg-white">
+            <div className="card p-6 border rounded-lg shadow-lg bg-neutral">
               <h3 className="text-xl font-bold mb-4 text-center">Enter Custom Amount</h3>
               <input
                 type="number"
-                className="w-full p-2 border rounded mb-4"
+                className="w-full p-2 border rounded mb-4 text-black"
                 placeholder="Enter amount"
                 value={customAmount}
                 onChange={handleCustomAmountChange}
               />
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
+                className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-orange-600 w-full"
                 onClick={handleCustomAmountDonate}
               >
                 Donate
