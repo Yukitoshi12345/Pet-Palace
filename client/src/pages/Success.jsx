620b927e51b55e2e024b24628107e2cab00a2b35
Jodie <jodielee062788@gmail.com>
2024-06-12 14:19:14 +1000

    update success page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Success = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-        <h2 className="text-2xl font-bold text-green-600 mb-4">Congratulations!</h2>
-        <p className="text-lg text-gray-700 mb-6">Your donation was successful.</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-neutral p-6 rounded-lg shadow-lg text-center max-w-lg">
+        <h2 className="text-[30px] font-bold mb-6 border-b p-2">Congratulations!</h2>
+        <p className="text-lg text-base-100 mb-4 text-center">Your donation was successful. Thank you for your generosity!</p>
+        <p className="text-lg text-base-100 mb-8 text-center">Your contribution will go a long way in helping us provide shelter, food, and medical care for
+          abandoned and rescued animals. With your support, we can continue to give these animals a second
+          chance at life. As a token of our appreciation, you will receive a free gift pack. Please check your
+          email for further details.</p>
         <Link
           to="/"
-          className="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600"
+          className="inline-block bg-yellow-500 text-white px-4 py-2 rounded-lg shadow hover:bg-orange-600"
         >
-          Go back to homepage
+          HOME
         </Link>
       </div>
     </div>
